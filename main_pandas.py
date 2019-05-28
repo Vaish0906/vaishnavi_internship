d30361ba551c22a042a09886ca459662d4795c73
Vaish0906 <38150305+Vaish0906@users.noreply.github.com>
2019-05-28 15:51:15 +0530

    2nd commit

--- main_pandas.py
+++ main_pandas.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-# -------question1--------------------
-
 df = pd.read_csv("Laliga.csv")
 #df = pd.read_csv("Laliga.csv", na_values = ['-'])
-df.replace(to_replace = '-',value = 0)
+df.Third = df.Third.astype(int)
+
+#df.replace(to_replace = '-', value = 0)
+print(df['Third'].dtype)
 print(df['Third'][12])
-print(df['Third'][3]+df['Third'][12])
+print(df['Third'][1]+df['Third'][12])
 
-#-------------------- question2--------------------------
-
+#--------------------question2--------------------------
 for i in df['Debut'] and j in df['Since/Last']:
   if i >=1930 and j <=1980:
     print(df['Team'])  
@@ -30,5 +30,24 @@ if
 
 #----------question 5--------------------
 
+df['Winning Percentage'] = df['GamesWon']/df['GamesPlayed']
+df['Winning Percentage'].multiply(100)
 
-df.loc[df['WinningPercentage'].idxmax()]
+df.loc[df['Winning Percentage'].idxmax()]
+
+#-------------QUESTION 6------------------  
+
+"""6.	Group teams based on their “Best position” and print the sum of their points for all positions (5 points)
+Eg: Best Position                Points 
+        1                              25000
+        2                              7000    
+  
+"""
+df = df.groupby('Best position')
+#df.get_group(1)
+
+for i in range[1:21]:
+if df.['Best position'][i] = 1:
+  result = df.get_group(i)
+  print()
+
